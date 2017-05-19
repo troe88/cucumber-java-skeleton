283287351dbcd21bd02b6ffa0871472fc9e68661
Dmitry Lebedev <dmitry.trok@gmail.com>
2017-05-19 15:46:54 +0300

    qwe

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,11 +1,24 @@
 package skeleton;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class Stepdefs {
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("^I wait (\\d+) hour$")
+    public void iWaitHour(int arg0) throws Throwable {
+    }
+
+    @Then("^my belly should growl$")
+    public void myBellyShouldGrowl() throws Throwable {
+        Assert.assertTrue(false);
     }
 }
