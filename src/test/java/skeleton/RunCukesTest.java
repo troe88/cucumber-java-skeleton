a601dabc945d587db440ca48c44ac33afceb0843
Dmitry Lebedev <dmitry.trok@gmail.com>
2017-05-19 15:55:52 +0300

    asd

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "junit:target/report.xml"})
+@CucumberOptions(plugin = {"pretty", "junit:target/report_new.xml"})
 public class RunCukesTest {
 }
